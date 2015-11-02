be2c04475d1e059b1dad63dbd2a508afde3cfc19
David R. Williamson <David@keepintouchwith.us>
2015-11-01 18:06:01 -0800

    prep for nuget push

--- AppSettings/AppSettings/Properties/AssemblyInfo.cs
+++ AppSettings/AppSettings/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("AppSettings")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Loads application settings into your own custom data class")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("AppSettings")]

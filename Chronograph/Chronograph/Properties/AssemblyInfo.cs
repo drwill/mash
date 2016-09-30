ea6d6b06e38f118e0ab0f653dacae9a91d7e36a2
David R. Williamson <drwill@microsoft.com>
2016-09-29 17:31:47 -0700

    Release chronograph officially

--- Chronograph/Chronograph/Properties/AssemblyInfo.cs
+++ Chronograph/Chronograph/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Mash Chronograph")]
 [assembly: AssemblyDescription("Keeps track of elapsed time, iteration count, max, and mean time spent for a given operation")]
 [assembly: AssemblyProduct("Mash.Chronograph")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -27,7 +27,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.1.*")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
 
 [assembly: InternalsVisibleTo("Mash.Chronograph.Tests")]

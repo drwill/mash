b28cfc3a26f0ae41e62b8d425444c177d6b545dd
David R. Williamson <david@keepintouchwith.us>
2016-11-23 21:17:31 -0800

    Add support for Development Mode where an additional SettingLoader can be specified to load development settings (if available) first

--- AppSettings/AppSettings/SettingTypeLoaders/SettingTypeModel.cs
+++ AppSettings/AppSettings/SettingTypeLoaders/SettingTypeModel.cs
@@ -5,6 +5,7 @@ namespace Mash.AppSettings
     internal class SettingTypeModel
     {
         internal ISettingLoader SettingLoader { get; set; }
+        internal ISettingLoader DevLoader { get; set; }
         internal dynamic SettingsClass { get; set; }
         internal PropertyInfo Member { get; set; }
         internal string SettingName { get; set; }

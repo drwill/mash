ef7f32e793a839ae31d59d172893c0c31d3d3685
David R. Williamson <david@keepintouchwith.us>
2016-11-24 13:20:34 -0800

    Add DevSettings project, and unit tests

---
+++ AppSettings/AppSettingsTests/DevSettingLoaderTests.cs
@@ -0,0 +1,42 @@
+﻿using Mash.AppSettings.DevSettings;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+namespace Mash.AppSettings.Tests
+{
+    [TestClass]
+    public class DevSettingLoaderTests
+    {
+        [TestMethod]
+        public void DevSettingLoader_Ctor_PathDefaultsToCurrentDir()
+        {
+            string file = "foo.json";
+            string expected = Path.Combine(Environment.CurrentDirectory, file);
+            var devSettingLoader = new DevSettingLoader(devSettingsFile: file);
+
+            Assert.AreEqual(expected, devSettingLoader.DevSettingFile);
+        }
+
+        [TestMethod]
+        public void DevSettingLoader_Ctor_FileDefaultsToUser()
+        {
+            string path = "c:\\foo";
+            string expected = Path.Combine(path, $"{Environment.UserName}.json");
+            var devSettingLoader = new DevSettingLoader(path);
+
+            Assert.AreEqual(expected, devSettingLoader.DevSettingFile);
+        }
+
+        [TestMethod]
+        public void DevSettingLoader_Ctor_FileAsSpecified()
+        {
+            string path = "c:\\foo";
+            string file = "foo.json";
+            string expected = Path.Combine(path, file);
+            var devSettingLoader = new DevSettingLoader(path, file);
+
+            Assert.AreEqual(expected, devSettingLoader.DevSettingFile);
+        }
+    }
+}
---
+++ AppSettings/DevSettings/DevSettingLoader.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Mash.AppSettings.DevSettings
+{
+    public class DevSettingLoader : ISettingLoader
+    {
+        public string DevSettingFile { get; private set; }
+
+        /// <summary>
+        /// Creates an instance of DevSettingLoader
+        /// </summary>
+        /// <param name="pathToDevSettingsFiles">The path to files holding developer-specific settings, defaults to current directory</param>
+        /// <param name="devSettingsFile">The developer's file to load, defaults to %username%.json</param>
+        public DevSettingLoader(string pathToDevSettingsFiles = null, string devSettingsFile = null)
+        {
+            if (String.IsNullOrEmpty(pathToDevSettingsFiles))
+            {
+                pathToDevSettingsFiles = Environment.CurrentDirectory;
+            }
+
+            if (String.IsNullOrWhiteSpace(devSettingsFile))
+            {
+                devSettingsFile = $"{Environment.UserName}.json";
+            }
+
+            DevSettingFile = Path.Combine(pathToDevSettingsFiles, devSettingsFile);
+        }
+
+        public string GetConnectionString(string connectionStringKey)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDictionary<string, string> GetConnectionStrings()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetSetting(string settingKey)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

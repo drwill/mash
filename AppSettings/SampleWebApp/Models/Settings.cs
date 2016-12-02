0bcbc9f78c644f17dde3dab8e921ad462f6101ed
David R. Williamson <david@keepintouchwith.us>
2016-12-01 16:35:07 -0800

    Reduce .net version, and employ DevSettings in example web project

--- AppSettings/SampleWebApp/Models/Settings.cs
+++ AppSettings/SampleWebApp/Models/Settings.cs
@@ -1,4 +1,5 @@
 ﻿using Mash.AppSettings;
+using Mash.AppSettings.DevSettings;
 using System;
 using System.Collections.Generic;
 
@@ -7,9 +8,33 @@ namespace SampleWebApp.Models
     /// <summary>
     /// Settings required for the running of this application
     /// </summary>
+    /// <remarks>
+    /// This example does not use SingletonSettings as a base class because it wants to optionally load developer-specific
+    /// settings.
+    /// </remarks>
     [AppSetting]
-    internal class Settings : SingletonSettings<Settings>
+    internal class Settings
     {
+        public static Settings Instance { get; } = Initialize();
+
+        private static Settings Initialize()
+        {
+            bool isDevMode = true; // Consider using an environment variable to indicate this
+            if (isDevMode)
+            {
+                // Normally we'd load a file based on the username so each developer would get their own settings,
+                // but for this example we'll specify the file name so everyone can see how this override works
+                AppSettingsLoader.DevSettings = new DevSettingLoader(
+                    dir: AppDomain.CurrentDomain.BaseDirectory,
+                    fileName: "common.json");
+            }
+
+            var settings = new Settings();
+            AppSettingsLoader.Load(Factory.GetAppConfigSettingLoader(), ref settings);
+
+            return settings;
+        }
+
         public string StringSetting { get; set; }
 
         [AppSetting(Key = "StringSettingOverride")]

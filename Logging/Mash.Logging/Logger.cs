d9f168a2d641967a3d1e74b934a659c2770064b3
David R. Williamson <drwill@microsoft.com>
2020-08-14 15:30:13 -0700

    Add a basic logger

---
+++ Logging/Mash.Logging/Logger.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+
+namespace Mash.Logging
+{
+    /// <summary>
+    /// Logger interface
+    /// </summary>
+    public class Logger
+    {
+        private readonly CentralLogger _centralLogger;
+
+        /// <summary>
+        /// Contextual properties to include with each log from this logger.
+        /// </summary>
+        public IDictionary<string, string> LoggerContext { get; } = new Dictionary<string, string>();
+
+        internal Logger(CentralLogger commonLogger, IDictionary<string, string> loggerContext)
+        {
+            _centralLogger = commonLogger;
+            if (loggerContext != null)
+            {
+                foreach (var kvp in loggerContext)
+                {
+                    LoggerContext.Add(kvp.Key, kvp.Value);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Clones the current logger and all properties.
+        /// </summary>
+        public virtual Logger Clone()
+        {
+            return new Logger(_centralLogger, LoggerContext);
+        }
+
+        /// <summary>
+        /// Log a trace message.
+        /// </summary>
+        /// <param name="message">The message to log</param>
+        /// <param name="severity">The severity level</param>
+        /// <param name="extraProperties">Additional properties to include</param>
+        public virtual void Trace(string message, TraceSeverity severity = TraceSeverity.Verbose, IDictionary<string, string> extraProperties = null)
+        {
+            _centralLogger.Trace(message, severity, _centralLogger.MergeProperties(LoggerContext, extraProperties));
+        }
+
+        /// <summary>
+        /// Logs a metric.
+        /// </summary>
+        /// <param name="name">The name of the metric</param>
+        /// <param name="value">The metric value</param>
+        /// <param name="extraProperties">Additional properties to include</param>
+        public virtual void Metric(string name, double value, IDictionary<string, string> extraProperties = null)
+        {
+            _centralLogger.Metric(name, value, _centralLogger.MergeProperties(LoggerContext, extraProperties));
+        }
+
+        /// <summary>
+        /// Logs an event.
+        /// </summary>
+        /// <param name="name">The name of the event</param>
+        /// <param name="extraProperties">Additional properties to include</param>
+        public virtual void Event(string name, IDictionary<string, string> extraProperties = null)
+        {
+            _centralLogger.Event(name, _centralLogger.MergeProperties(LoggerContext, extraProperties));
+        }
+
+        /// <summary>
+        /// Flushes any logs. Use before closing the process to ensure all information has been logged.
+        /// </summary>
+        public virtual void Flush()
+        {
+            _centralLogger.Flush();
+        }
+    }
+}
---
+++ Logging/readme.md
@@ -0,0 +1,46 @@
+# Mash.Logging
+
+Most of my code starts running locally in a console window, so I want logs printed to the screen.
+Soon that code begins running headless, and I rely on logs to know what happened after the fact.
+I don't want to change my logging up, I just want it to go to a different location.
+Sometimes, I want it to go to multiple locations.
+
+This utility allows you to configure logging providers, and build loggers from that to hand off to business logic.
+It also lets you specify a common property bag of data to be automatically included with each log statement.
+Additionally, each logger instance can build its own context.
+
+## Initialization
+
+To initialize logging:
+
+```csharp
+var loggingBuilder = new LoggingBuilder();
+
+// Add any app-level contextual properties
+loggingBuilder.AppContext.Add("RunId", Guid.NewGuid().ToString());
+
+// Add desired providers
+loggingBuilder.LogProviders.Add(new ConsoleLogProvider());
+
+var logger = loggingBuilder.BuildLogger(
+    new Dictionary<string, string>
+    {
+        { "Operation", "ProcessOrders" },
+    });
+
+// Now pass off your logger to code to do work
+ProcessOrders(logger);
+```
+
+## Logging
+
+Logging is simple.
+Code can log traces, metrics, or events.
+See this [sample](./Sample/Program.cs) for examples.
+
+If an operation branches off into more than one code path, you can clone the current logger which copies the current logger context into the new logger.
+Modifications to that logger's context will not affect the current one.
+
+## Logging providers
+
+This utility includes a console logger, and you can build your own adapters to other destinations.
